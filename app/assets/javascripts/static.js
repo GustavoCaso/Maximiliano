83b9ab99984464f0a14777ae9df9191a1859f7cb
Gustavo Caso <gustavocaso@gmail.com>
2013-12-23 18:44:31 +0100

    Adding small menu functionality

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
 
   });
 
-/*    $('.small-menu-list').hide();
+    $('.small-menu-list').hide();
 
   $('.small-menu-button').on('click', function(){
     $('.small-menu-list').slideToggle();
   });
-*/
+
 });
