2791d9961bca4cba422df4802413e02be7d3b84c
GustavoCaso <gustavocaso@gmail.com>
2013-12-27 11:13:50 +0100

    Added new configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,12 @@ Maximiliano::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
