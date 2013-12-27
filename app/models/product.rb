41c3b2ffa32d42a8ec6269da9a69074774e98135
GustavoCaso <gustavocaso@gmail.com>
2013-12-27 13:04:40 +0100

    Misspellled a coma

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ActiveRecord::Base
                     },
                     :default_url => "no_photo.png",
                     :storage => :s3,
-                    :bucket => ENV['S3_BUCKET_NAME']
+                    :bucket => ENV['S3_BUCKET_NAME'],
                     :s3_credentials => {
                       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
